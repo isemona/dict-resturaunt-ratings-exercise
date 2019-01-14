e51460ec21eaf0643ffafa5e824076f5ba73c2eb
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-14 12:52:53 -0800

    Added restaurant-rating.py

--- ratings.py
+++ ratings.py
@@ -9,6 +9,7 @@ ratings = {}
 for line in open(filename):
     new_tup = tuple(line.rstrip().split(":"))
     ratings[new_tup[0]] = new_tup[1]
+filename.close()
 
 for key,value in sorted(ratings.items()):
     print("{} is rated at {}.".format(key,value))
